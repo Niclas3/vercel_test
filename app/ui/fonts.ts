0324cc0982a9bdd0a57318e202b7387437bb2384
zm <missingurchin@gmail.com>
2025-04-01 22:38:10 +0800

    feat: add subset on font butcherman

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -4,6 +4,6 @@ export const inter = Inter({subsets :['latin']});
 export const lustria = Lustria({weight: "400"});
 export const lusitana = Lusitana({weight : ["400", "700"]});
 // Scary Halloween Font
-export const scary = Butcherman({weight :"400"});
+export const scary = Butcherman({weight :"400", subsets:['latin']});
 
 
