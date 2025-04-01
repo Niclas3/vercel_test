c970b9225928ef066756e4b518c06e842c62cdaf
zm <missingurchin@gmail.com>
2025-04-01 22:41:00 +0800

    fix(font): add subsets for all fonts (lustria,lusitana)

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,8 +1,8 @@
 import { Inter, Lustria, Butcherman, Lusitana } from 'next/font/google';
 
 export const inter = Inter({subsets :['latin']});
-export const lustria = Lustria({weight: "400"});
-export const lusitana = Lusitana({weight : ["400", "700"]});
+export const lustria = Lustria({weight: "400", subsets:['latin']});
+export const lusitana = Lusitana({weight : ["400", "700"], subsets:['latin']});
 // Scary Halloween Font
 export const scary = Butcherman({weight :"400", subsets:['latin']});
 
