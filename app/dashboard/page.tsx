5e42162cc40732f69070ab24bd89d8fe8690945a
zm <missingurchin@gmail.com>
2025-04-01 22:29:37 +0800

    feat: init app

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,6 @@
+export default function Page(){
+
+          return <p className="inline-block text-gray-400 hover:text-red-400"> aaaaaaaaaaa </p>
+
+  // return <p className="text-gray-500 hover:text-red-500">悬停我，我会变红！</p>
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter, Lustria, Butcherman, Lusitana } from 'next/font/google';
+
+export const inter = Inter({subsets :['latin']});
+export const lustria = Lustria({weight: "400"});
+export const lusitana = Lusitana({weight : ["400", "700"]});
+// Scary Halloween Font
+export const scary = Butcherman({weight :"400"});
+
+
