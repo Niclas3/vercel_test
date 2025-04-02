403cb1b6eb75ea5a6b0efb69f5c18bbe8bf09edd
zm <missingurchin@gmail.com>
2025-04-02 21:50:43 +0800

    feat:from chapter6 to chapter11

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -9,19 +9,23 @@ import {
 } from './definitions';
 import { formatCurrency } from './utils';
 
+
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+
+import { unstable_noStore as noStore } from 'next/cache';
 
 export async function fetchRevenue() {
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    // noStore();
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+    console.log('Data fetch completed after 3 seconds.');
 
     return data;
   } catch (error) {
